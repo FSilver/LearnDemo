eaa819ed3fce9972abed1ec54085b3b00c9f44c1
fangwei <740987339@qq.com>
2017-11-22 13:16:47 +0800

    added: 搭建测试基本工程

---
+++ LearnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnDemo
+//
+//  Created by Lizhi on 2017/11/22.
+//  Copyright © 2017年 WSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnDemo
+//
+//  Created by Lizhi on 2017/11/22.
+//  Copyright © 2017年 WSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
