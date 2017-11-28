fca8bd4b1a30b5f729980d7ed7ffb11fca8c6ae9
fangwei <740987339@qq.com>
2017-11-29 00:09:14 +0800

    added: test SDWebImage

--- LearnDemo/ViewController.m
+++ LearnDemo/ViewController.m
@@ -7,18 +7,40 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+WebCache.h"
 
 @interface ViewController ()
-
+{
+    UIImageView *_imageView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame = CGRectMake(100, 400, 100, 30);
+    [btn setTitle:@"test" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(hello) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)hello {
+    
+    if(!_imageView){
+        _imageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 100, 336, 210)];
+        [self.view addSubview:_imageView];
+    }
+   [_imageView sd_setImageWithURL:[NSURL URLWithString:@"http://47.88.148.22/car.jpg"] placeholderImage:[UIImage imageNamed:@"1.png"]];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
